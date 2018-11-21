119265b471aa97bb8e18a03ffc36af33c61030d1
nkottapalli <nishanth@thezerogames.com>
2018-11-22 03:22:45 +0530

    added enumerated list specifications 5.3, 5.4 as per openrtb api specification 2.5 final

--- api/final/v25/openrtbv25.go
+++ api/final/v25/openrtbv25.go
@@ -1,1 +1,33 @@
 package openrtbv25
+
+// 5.3 Creative Attributes
+const (
+	CreativeAttributeAudioAdAutoPlay                 = 1
+	CreativeAttributeAudioAdUserInitiated            = 2
+	CreativeAttributeExpandableAuto                  = 3
+	CreativeAttributeExpandableUserInitiatedClick    = 4
+	CreativeAttributeExpandableUserInitiatedRollover = 5
+	CreativeAttributeInBannerVideoAdAutoPlay         = 6
+	CreativeAttributeInBannerVideoAdUserInitiated    = 7
+	CreativeAttributePop                             = 8
+	CreativeAttributeProvocativeOrSuggestiveImagery  = 9
+	CreativeAttributeExtremeAnimation                = 10
+	CreativeAttributeSurveys                         = 11
+	CreativeAttributeTextOnly                        = 12
+	CreativeAttributeUserInitiated                   = 13
+	CreativeAttributeWindowsDialogOrAlert            = 14
+	CreativeAttributeHasAudioWithPlayer              = 15
+	CreativeAttributeAdProvidesSkipButton            = 16
+	CreativeAttributeAdobeFlash                      = 17
+)
+
+// 5.4 Ad Position
+const (
+	AdPosUnknown    = 0
+	AdPosAboveFold  = 1
+	AdPosBelowFold  = 3
+	AdPosHeader     = 4
+	AdPosFooter     = 5
+	AdPosSidebar    = 6
+	AdPosFullscreen = 7
+)
